7a16869deb04fd0857ea26ccc64ce9761ed82a22
Jose Francisco de Almeida Filho <69639482+josefralmeidaads@users.noreply.github.com>
2020-08-14 13:36:30 -0300

    Web

--- web/src/App.js
+++ web/src/App.js
@@ -1,13 +1,48 @@
 import React from 'react';
-
-//Component
-//Estado
-//Propriedade
+import Button from '@material-ui/core/Button';
+import Icon from '@material-ui/core/Icon';
+import SaveIcon from '@material-ui/icons/Save';
+import './global.css'
+import './App.css'
+import './SideBar.css'
 
 function App() {
-  return (
-    <h1>Hello World</h1>
-  );
+  
+  return(
+    <div id="app"> 
+      <aside>
+            <strong>Cadastrar</strong>
+            <form>
+                <div class="input-block">
+                    <label htmlFor="github_username">Usuário do GitHub</label>
+                    <input name="github_username" id="github_username" required></input>
+                </div>
+
+                <div class="input-block">
+                  <label htmlFor="techs">Tecnologias</label>
+                  <input name="techs" id="techs" required></input>
+                </div>
+
+              <div className="input-group">
+                <div class="input-block">
+                  <label htmlFor="latitude">Latitude</label>
+                  <input name="latitude" id="latitude" required></input>
+                </div>
+              
+                <div class="input-block">
+                  <label htmlFor="longitude">Longitude</label>
+                  <input name="longitude" id="longitude" required></input>
+                </div>
+              </div>    
+              <Button variant="contained" color="secondary" type="submit" startIcon={<SaveIcon />}>Salvar</Button>
+            </form>
+      </aside>
+      <main>
+
+      </main>
+    </div>
+  )
+  
 }
 
 export default App;
